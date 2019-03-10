fa5b97de747526e0334305350bbddd7b84cc0f74
aerych <aerychj@gmail.com>
2019-03-10 15:34:53 -0500

    CoreData: Configures the basic core data stack + tests.

--- Newspack/Newspack/System/CoreDataManager.swift
+++ Newspack/Newspack/System/CoreDataManager.swift
@@ -1,45 +1,102 @@
 import Foundation
 import CoreData
 
-class CoreDataManager {
+// PersistentContainerManager is a protocol specifying a single method for
+// creating and returning an NSPersistentContainer.
+// This protocol, and its default extension, is used to define a default
+// NSPersistentContainer for CoreDataManager that can be overridden by
+// tests that provide their own implementation via an extension to
+// CoreDataManager.
+//
+protocol PersistentContainerManager {
 
-    let name: String
+    // Create and return an NSPersistentContainer.
+    //
+    func createContainer() -> NSPersistentContainer
+}
 
-    private init(name: String) {
-        self.name = name
+// A protocol extension providing a default implementation of the createContainer
+// method.
+//
+extension PersistentContainerManager {
+    private var name: String {
+        return "Newspack"
     }
 
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
+    func createContainer() -> NSPersistentContainer {
         let container = NSPersistentContainer(name: name)
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         return container
+    }
+}
+
+// CoreDataManager is a singleton wrapper around a NSPersistentContainer.
+// Its responsible for providing NSManagedObjectContexts and saving contexts.
+//
+class CoreDataManager: PersistentContainerManager {
+
+    // The shared singleton instance of the CoreDataManager.
+    //
+    static let shared = CoreDataManager()
+
+    // Private lazy constructor for the internally managed NSPersistentContainer
+    //
+    private lazy var container: NSPersistentContainer = {
+        return createContainer()
     }()
 
-    // MARK: - Core Data Saving support
+    // The main NSManagedObjectContext, Its operations are performed on the UI thread.
+    // It is a child context of a private background context performing IO.
+    //
+    public var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
 
-    func saveContext () {
-        let context = persistentContainer.viewContext
+    // A convenience method for performing work in the supplied block on a
+    // background thread.
+    //
+    // Parameters:
+    // - block: An anonymous block exectued on a background thread.
+    //
+    public func performBackgroundTask(_ block: @escaping (NSManagedObjectContext) -> Void) {
+        container.performBackgroundTask { context in
+            block(context)
+        }
+    }
+
+    // Returns an NSManagedObjectContext that is a child of the mainContext and
+    // is configured to run on a private background queue.
+    //
+    public func newPrivateChildContext() -> NSManagedObjectContext {
+        let childContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        childContext.parent = mainContext
+        childContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        return childContext
+    }
+
+    // Returns an NSManagedObjectContext that is a child of the NSPersistentContiner's
+    // private context--a sibling of the public mainContext.
+    //
+    public func newPrivateContext() -> NSManagedObjectContext {
+        return container.newBackgroundContext()
+    }
+
+    // A convenience method for saving the mainContext.
+    //
+    public func saveContext() {
+        saveContext(context: mainContext)
+    }
+
+    // Saves the passed NSManagedObjectContext if it has changes.
+    //
+    // Parameters
+    // - context: The NSManagedObjectContext to save.
+    //
+    public func saveContext(context: NSManagedObjectContext) {
         if context.hasChanges {
             do {
                 try context.save()
---
+++ Newspack/NewspackTests/CoreData/CoreDataManagerTests.swift
@@ -0,0 +1,38 @@
+import XCTest
+import CoreData
+@testable import Newspack
+
+class CoreDataManagerTests: BaseTest {
+
+    // Check that the context is a child of the main context.
+    //
+    func testPrivateChildContextIsChildOfMainContext() {
+        let context = CoreDataManager.shared.newPrivateChildContext()
+
+        XCTAssertTrue(context.parent == CoreDataManager.shared.mainContext)
+        XCTAssertTrue(context.concurrencyType == .privateQueueConcurrencyType)
+    }
+
+    // CHeck that the context is a private sibling of the public main context.
+    //
+    func testPrivateContextIsSiblingOfMainContext() {
+        let context = CoreDataManager.shared.newPrivateContext()
+
+        XCTAssertTrue(context.parent == CoreDataManager.shared.mainContext.parent)
+        XCTAssertTrue(context.concurrencyType == .privateQueueConcurrencyType)
+    }
+
+    // Check that blocks are ran on a background thread.
+    //
+    func testPerformBackgroundTaskIsRanInBackground() {
+        let expectation = XCTestExpectation(description: "Check if background thread")
+
+        CoreDataManager.shared.performBackgroundTask { (context) in
+            XCTAssertFalse(Thread.isMainThread)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5.0)
+    }
+
+}
