15889a84eb22d11c5eba97865645dffaacf367d0
aerych <aerychj@gmail.com>
2019-04-01 16:52:24 -0500

    SessionManager: Refactor to singleton and instantiate the API.

--- Newspack/Newspack/System/SessionManager.swift
+++ Newspack/Newspack/System/SessionManager.swift
@@ -1,17 +1,37 @@
 import Foundation
+import WordPressKit
 
 /// SessionManager is responsible for defining the current session for the
 /// current account.  It serves to decouple the API and Store layers.
 ///
-struct SessionManager {
+class SessionManager {
 
-    var token: String
+    /// Singleon reference
+    ///
+    static let shared = SessionManager()
 
-    init?(account: Account) {
+    /// A readonly reference to the api.
+    /// The API is an anonymous API until initialzed with an Account
+    ///
+    private(set) var api = WordPressComRestApi(oAuthToken: nil, userAgent: UserAgent.defaultUserAgent)
+
+    private init() {}
+
+    /// Initialize the session with the specified account.  Typically this will
+    /// be the current account from the AccountStore
+    ///
+    /// - Parameter account: The account for the session. All api calls will be made
+    /// on behalf of the account.
+    ///
+    @discardableResult
+    func initialize(account: Account) -> Bool {
         let store = StoreContainer.shared.accountStore
         guard let token = store.getAuthTokenForAccount(account) else {
-            return nil
+            return false
         }
-        self.token = token
+
+        api = WordPressComRestApi(oAuthToken: token, userAgent: UserAgent.defaultUserAgent)
+        return true
     }
+
 }
