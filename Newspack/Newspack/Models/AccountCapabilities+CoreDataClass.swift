ce019b0626d15ff386ca5ea1f889ff86ff36245a
aerych <aerychj@gmail.com>
2019-06-11 14:08:50 -0500

    AccountCapabilities: Clean up orphan entities.

--- Newspack/Newspack/Models/AccountCapabilities+CoreDataClass.swift
+++ Newspack/Newspack/Models/AccountCapabilities+CoreDataClass.swift
@@ -8,4 +8,11 @@ public class AccountCapabilities: NSManagedObject {
         return NSFetchRequest<AccountCapabilities>(entityName: "AccountCapabilities")
     }
 
+    public override func willSave() {
+        /// Prevent orphaned entities. If we ever save without a relationship
+        /// to a site just delete.
+        if site == nil && !isDeleted {
+            managedObjectContext?.delete(self)
+        }
+    }
 }
