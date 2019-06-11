a7dd8733c2deccd18509c702cac4331ab145750b
aerych <aerychj@gmail.com>
2019-06-11 13:21:53 -0500

    Adds a comment to clarify method intent.

--- Newspack/Newspack/Models/AccountDetails+CoreDataClass.swift
+++ Newspack/Newspack/Models/AccountDetails+CoreDataClass.swift
@@ -9,6 +9,8 @@ public class AccountDetails: NSManagedObject {
     }
 
     public override func willSave() {
+        /// Prevent orphaned entities. If we ever save without a relationship
+        /// to an account just delete.
         if account == nil && !isDeleted {
             managedObjectContext?.delete(self)
         }
