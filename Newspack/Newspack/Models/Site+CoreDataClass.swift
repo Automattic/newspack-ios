eef9a229bf3a2028ec6e589997897a18b21012fb
aerych <aerychj@gmail.com>
2019-06-11 14:10:18 -0500

    Site: Adds some convenience methods for checking capabilities and roles

--- Newspack/Newspack/Models/Site+CoreDataClass.swift
+++ Newspack/Newspack/Models/Site+CoreDataClass.swift
@@ -8,4 +8,27 @@ public class Site: NSManagedObject {
         return NSFetchRequest<Site>(entityName: "Site")
     }
 
+
+    /// Check for the specified capability.
+    ///
+    /// - Parameter string: The name of the capability to look for.
+    /// - Returns: Returns true if the account has the specified capability.
+    func hasCapability(string: String) -> Bool {
+        if let cap = capabilities?.capabilities[string.lowercased()] {
+            return cap
+        }
+        return false
+    }
+
+
+    /// Check if the specified role is included.
+    ///
+    /// - Parameter string: The role to look for.
+    /// - Returns: True if the role was found, false otherwise.
+    func hasRole(string: String) -> Bool {
+        if let capabilities = capabilities {
+            return capabilities.roles.contains(string.lowercased())
+        }
+        return false
+    }
 }
