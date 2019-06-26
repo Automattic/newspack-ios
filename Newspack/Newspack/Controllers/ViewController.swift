a866962dc7b1b3bae3211a7808f58f6345bcd7e4
aerych <aerychj@gmail.com>
2019-06-26 15:44:36 -0500

    UI: Show menu when session is initialized.

--- Newspack/Newspack/Controllers/ViewController.swift
+++ Newspack/Newspack/Controllers/ViewController.swift
@@ -1,16 +1,28 @@
 import UIKit
+import WordPressFlux
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
-    }
+    var receipt: Receipt?
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        checkSession()
+    }
 
+    func checkSession() {
+        guard SessionManager.shared.state == .initialized else {
+            receipt = SessionManager.shared.onChange({
+                self.checkSession()
+            })
+            return
+        }
+
+        receipt = nil
+
+        let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SiteMenuViewController")
+        controller.modalTransitionStyle = .crossDissolve
+        navigationController?.setViewControllers([controller], animated: true)
     }
 
 }
