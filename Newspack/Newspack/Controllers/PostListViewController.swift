725717c8d5391f2452cce57f43e3457eba2bca77
aerych <aerychj@gmail.com>
2019-06-28 17:05:01 -0500

    Posts: Fetch posts and populate cells. Super simple.

--- Newspack/Newspack/Controllers/PostListViewController.swift
+++ Newspack/Newspack/Controllers/PostListViewController.swift
@@ -1,6 +1,29 @@
 import UIKit
+import CoreData
+import WordPressFlux
 
 class PostListViewController: UITableViewController {
+
+    var receipt:Receipt?
+
+    lazy var resultsController: NSFetchedResultsController<Post> = {
+        let context = CoreDataManager.shared.mainContext
+        let fetchRequest = Post.defaultFetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "postID", ascending: false)]
+        return NSFetchedResultsController(fetchRequest: fetchRequest,
+                                          managedObjectContext: context,
+                                          sectionNameKeyPath: nil,
+                                          cacheName: nil)
+    }()
+
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        receipt = StoreContainer.shared.postStore.onChange {
+            self.handlePostsChanged()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -8,62 +31,32 @@ class PostListViewController: UITableViewController {
         clearsSelectionOnViewWillAppear = false
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        // Sync if needed.
+        StoreContainer.shared.postStore.syncPosts()
+        handlePostsChanged()
+    }
+
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return resultsController.fetchedObjects?.count ?? 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCellIdentifier", for: indexPath)
 
-        // Configure the cell...
+        let post = resultsController.object(at: indexPath)
+        cell.textLabel?.text = post.titleRendered
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -75,4 +68,11 @@ class PostListViewController: UITableViewController {
     }
     */
 
+
+    func handlePostsChanged() {
+        try? resultsController.performFetch()
+        tableView.reloadData()
+    }
 }
+
+
