32e0a9fc90633b799aa7b08b3b139d37982383f7
aerych <aerychj@gmail.com>
2019-07-30 20:24:49 -0500

    PostListItem: Adds an isStale check

--- Newspack/Newspack/Models/PostListItem+CoreDataClass.swift
+++ Newspack/Newspack/Models/PostListItem+CoreDataClass.swift
@@ -15,4 +15,17 @@ public class PostListItem: NSManagedObject {
             managedObjectContext?.delete(self)
         }
     }
+
+
+    /// Checks if the associated posts is out of date.
+    ///
+    /// - Returns: True if the associated post is stale / out of date.
+    ///
+    func isStale() -> Bool {
+        guard let p = post else {
+            return true
+        }
+        return (revisionCount != p.revisionCount) || (modifiedGMT.compare(p.modifiedGMT) != .orderedSame)
+    }
+
 }
