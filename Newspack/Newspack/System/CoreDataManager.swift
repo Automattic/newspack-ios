b44847c22122aa77a49c345195ca6f48fa01e1eb
aerych <aerychj@gmail.com>
2019-08-06 20:09:22 -0500

    CoreData: Perform save operations in a wait block.

--- Newspack/Newspack/System/CoreDataManager.swift
+++ Newspack/Newspack/System/CoreDataManager.swift
@@ -82,13 +82,15 @@ class CoreDataManager {
     //
     public func saveContext(context: NSManagedObjectContext) {
         if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            context.performAndWait {
+                do {
+                    try context.save()
+                } catch {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    let nserror = error as NSError
+                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                }
             }
         }
     }
