a626332c9a0e6e1043ab87e9ec4a782860a57bb3
aerych <aerychj@gmail.com>
2019-08-07 13:18:39 -0500

    API: Remove unused action.

--- Newspack/Newspack/Api/Actions/PostApiActions.swift
+++ Newspack/Newspack/Api/Actions/PostApiActions.swift
@@ -1,13 +1,6 @@
 import Foundation
 import WordPressFlux
 
-struct PostsFetchedApiAction: ApiAction {
-    var payload: [RemotePost]?
-    var error: Error?
-    var siteUUID: UUID
-    // TOOD: Add meta about the fetch. e.g. offset|page, search, filter, etc.
-}
-
 struct PostFetchedApiAction: ApiAction {
     var payload: RemotePost?
     var error: Error?
