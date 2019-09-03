f9c8c4750c4e42b25432331e6af3263f092f1cff
aerych <aerychj@gmail.com>
2019-09-03 14:32:04 -0500

    Editor: Basic autosave and post creation logic.

--- Newspack/Newspack/Stores/EditCoordinator.swift
+++ Newspack/Newspack/Stores/EditCoordinator.swift
@@ -7,11 +7,13 @@ import Aztec
 ///
 class EditCoordinator: Store {
 
+    private let currentSiteID: UUID
     var postItem: PostListItem?
     let stagedEdits: StagedEdits
-    var draftUUID: UUID?
+    var draftUUID: UUID? // An identifier used to pair an api action to edits being made.
 
-    init(postItem: PostListItem?, dispatcher: ActionDispatcher) {
+    init(postItem: PostListItem?, dispatcher: ActionDispatcher, siteID: UUID) {
+        self.currentSiteID = siteID
         self.postItem = postItem
         self.stagedEdits = postItem?.stagedEdits ?? StagedEdits(context: CoreDataManager.shared.mainContext)
         super.init(dispatcher: dispatcher)
@@ -87,16 +89,106 @@ class EditCoordinator: Store {
 // MARK: - Api action handlers
 extension EditCoordinator {
     func handleAutosaveApiAction(action: AutosaveApiAction) {
+        if action.isError() {
+            // TODO:
+            return
+        }
 
+        guard let remoteRevision = action.payload else {
+            // If the payload is empty, the post is either draft or pending and
+            // was directly updated by the autosave.
+            return
+        }
+
+        guard let post = stagedEdits.postListItem?.post else {
+            // this shouldn't happen.
+            return
+        }
+
+        // create or update autosave revision
+        let context = CoreDataManager.shared.mainContext
+        let fetchRequest = Revision.defaultFetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "post == %@ AND revisionID == %ld", post, remoteRevision.revisionID)
+
+        let revision: Revision
+        do {
+            revision = try context.fetch(fetchRequest).first ?? Revision(context: context)
+        } catch {
+            revision = Revision(context: context)
+            // TODO: properly log this
+        }
+
+        updateRevision(revision: revision, with: remoteRevision)
+        revision.post = post
+        CoreDataManager.shared.saveContext()
     }
 
+
     func handlePostCreatedApiAction(action: PostCreatedApiAction) {
+        if action.isError() {
+            // TODO:
+            return
+        }
+
+        guard
+            let remotePost = action.payload,
+            action.uuid == draftUUID
+        else {
+            return
+        }
+
+        draftUUID = nil
+
+        // Use the payload to create a new Post and PostListItem.
+        // The item should be assigned to the "ALL" PostList.
+
+        guard let list = StoreContainer.shared.postListStore.postListByName(name: "all", siteUUID: currentSiteID) else {
+            return
+        }
+
+        let context = CoreDataManager.shared.mainContext
+
+        let postStore = StoreContainer.shared.postStore
+        let post = Post(context: context)
+        postStore.updatePost(post, with: remotePost)
+        post.site = list.site
+
+        let postItem = PostListItem(context: context)
+        postItem.postID = post.postID
+        postItem.dateGMT = post.dateGMT
+        postItem.modifiedGMT = post.modifiedGMT
+        postItem.revisionCount = 0
+
+        postItem.stagedEdits = stagedEdits
+        postItem.post = post
+        postItem.site = list.site
+        postItem.addToPostLists(list)
 
     }
 
     func handlePostUpdatedApiAction(action: PostUpdatedApiAction) {
+        // TODO:
 
     }
+
+
+    func updateRevision(revision:Revision, with remoteRevision: RemoteRevision) {
+        revision.authorID = remoteRevision.authorID
+        revision.content = remoteRevision.content
+        revision.contentRendered = remoteRevision.contentRendered
+        revision.date = remoteRevision.date
+        revision.dateGMT = remoteRevision.dateGMT
+        revision.excerpt = remoteRevision.excerpt
+        revision.excerptRendered = remoteRevision.excerptRendered
+        revision.modified = remoteRevision.modified
+        revision.modifiedGMT = remoteRevision.modifiedGMT
+        revision.parentID = remoteRevision.parentID
+        revision.revisionID = remoteRevision.revisionID
+        revision.slug = remoteRevision.slug
+        revision.title = remoteRevision.title
+        revision.titleRendered = remoteRevision.titleRendered
+    }
+
 }
 
 extension EditCoordinator: GutenbergBridgeDataSource {
