98cb4f83e40758c25f847b3837e57bad4b873f2e
aerych <aerychj@gmail.com>
2019-09-04 15:35:40 -0500

    Data: Add lastModified property to StagedEdits

--- Newspack/Newspack/Models/StagedEdits+CoreDataProperties.swift
+++ Newspack/Newspack/Models/StagedEdits+CoreDataProperties.swift
@@ -11,6 +11,7 @@ extension StagedEdits {
     @NSManaged public var title: String?
     @NSManaged public var content: String?
     @NSManaged public var excerpt: String?
+    @NSManaged public var lastModified: Date
     @NSManaged public var postListItem: PostListItem?
 
 }
