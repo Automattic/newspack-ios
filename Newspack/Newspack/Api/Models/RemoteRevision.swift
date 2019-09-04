ad9bca36892314f9a9d457191adadf7618ca2d59
aerych <aerychj@gmail.com>
2019-09-04 13:44:33 -0500

    API: Handle null date/date_gmt returned from autosave endpoint.

--- Newspack/Newspack/Api/Models/RemoteRevision.swift
+++ Newspack/Newspack/Api/Models/RemoteRevision.swift
@@ -24,20 +24,38 @@ struct RemoteRevision {
     /// - Parameter dict: The source dictionary
     ///
     init(dict: [String: AnyObject]) {
+        // The core rest api's posts/ID/autosave endpoint can return null for date and date_gmt
+        // if the post is a draft and a date/date_gmt has not previously been set.
+        // In these cases, if you check the result of the /posts/ID endpoint result
+        // you should see that the value of date/date_gmt matches the value of
+        // modified/modified_gmt.  Based on this we'll use the modified/modified_gmt
+        // values for date/date_gmt whenever date/date_gmt is null.
+        let dateModifiedStr = dict[stringForKey: "modified"]
+        let dateModifiedGMT = Date.dateFromGMTString(string: dict[stringForKey: "modified_gmt"])!
+        let dateStr = dict[stringForKey: "date"]
+        let maybeDateGMT = Date.dateFromGMTString(string: dict[stringForKey: "date_gmt"])
+
         revisionID = dict[intForKey: "id"]
         authorID = dict[intForKey: "author"]
         content = dict[stringAtKeyPath: "content.raw"]
         contentRendered = dict[stringAtKeyPath: "content.rendered"]
-        date = dict[stringForKey: "date"]
-        dateGMT = Date.dateFromGMTString(string: dict[stringForKey: "date_gmt"])!
+
+        if let d = maybeDateGMT {
+            date = dateStr
+            dateGMT = d
+        } else {
+            date = dateModifiedStr
+            dateGMT = dateModifiedGMT
+        }
+
         excerpt = dict[stringAtKeyPath: "excerpt.raw"]
         excerptRendered = dict[stringAtKeyPath: "excerpt.rendered"]
         guid = dict[stringAtKeyPath: "guid.raw"]
         guidRendered = dict[stringAtKeyPath: "guid.rendered"]
         parentID = dict[intForKey: "parent"]
         previewLink = dict[stringForKey: "preview_link"]
-        modified = dict[stringForKey: "modified"]
-        modifiedGMT = Date.dateFromGMTString(string: dict[stringForKey: "modified_gmt"])!
+        modified = dateModifiedStr
+        modifiedGMT = dateModifiedGMT
         slug = dict[stringForKey: "slug"]
         title = dict[stringAtKeyPath: "title.raw"]
         titleRendered = dict[stringAtKeyPath: "title.rendered"]
