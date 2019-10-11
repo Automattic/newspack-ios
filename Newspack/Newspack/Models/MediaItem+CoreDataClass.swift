f7932a19f877f9f1f85d31340d181893f56fa007
aerych <aerychj@gmail.com>
2019-10-11 14:27:12 -0500

    MediaItem: Adds convenience method to check if stale.

--- Newspack/Newspack/Models/MediaItem+CoreDataClass.swift
+++ Newspack/Newspack/Models/MediaItem+CoreDataClass.swift
@@ -8,4 +8,14 @@ public class MediaItem: NSManagedObject {
         return NSFetchRequest<MediaItem>(entityName: "MediaItem")
     }
 
+    /// Checks if the associated media is out of date.
+    ///
+    /// - Returns: True if the associated media is stale / out of date.
+    ///
+    func isStale() -> Bool {
+        guard let m = media else {
+            return true
+        }
+        return modifiedGMT.compare(m.modifiedGMT) != .orderedSame
+    }
 }
