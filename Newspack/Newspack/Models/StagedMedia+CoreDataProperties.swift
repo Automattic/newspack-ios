7c92834d897daa3cdd6d9aed9a11322282c11842
aerych <aerychj@gmail.com>
2019-11-20 20:20:01 -0600

    StagedMedia: Unwrap non-optional properties.

--- Newspack/Newspack/Models/StagedMedia+CoreDataProperties.swift
+++ Newspack/Newspack/Models/StagedMedia+CoreDataProperties.swift
@@ -8,7 +8,7 @@ extension StagedMedia {
         return NSFetchRequest<StagedMedia>(entityName: "StagedMedia")
     }
 
-    @NSManaged public var uuid: UUID?
+    @NSManaged public var uuid: UUID!
     @NSManaged public var localFilePath: String?
     @NSManaged public var assetIdentifier: String?
     @NSManaged public var mediaType: String?
@@ -17,6 +17,6 @@ extension StagedMedia {
     @NSManaged public var altText: String?
     @NSManaged public var caption: String?
     @NSManaged public var originalFileName: String?
-    @NSManaged public var site: Site?
+    @NSManaged public var site: Site!
 
 }
