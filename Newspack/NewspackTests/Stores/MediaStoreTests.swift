29922a8ce80e305549db301dbe1bc6d56e194d80
aerych <aerychj@gmail.com>
2019-12-18 17:11:26 -0600

    Tests: Adds media store tests.

--- Newspack/NewspackTests/Stores/MediaStoreTests.swift
+++ Newspack/NewspackTests/Stores/MediaStoreTests.swift
@@ -1,24 +1,93 @@
 import XCTest
 import CoreData
-import WordPressFlux
 @testable import Newspack
 
 class MediaStoreTests: BaseTest {
 
+    let remoteMediaEditFile = "remote-media-edit.json"
+
+    var account: Account?
+    var site: Site?
+    var store: MediaStore?
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+
+        // Test account
+        account = accountStore!.createAccount(authToken: "testToken", forNetworkAt: "example.com")
+
+        // Test site
+        let context = CoreDataManager.shared.mainContext
+        site = ModelFactory.getTestSite(context: context)
+        site!.account = account
+
+        // Test store
+        store = MediaStore(dispatcher: .global, siteID: site!.uuid)
+        CoreDataManager.shared.saveContext(context: context)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+
+        account = nil
+        site = nil
+        store = nil
     }
 
     func testGetMediaItemWithID() {
+        let context = CoreDataManager.shared.mainContext
+        let mediaItemStore = MediaItemStore()
+        guard let response = Loader.jsonObject(for: "remote-media-edit") as? [String: AnyObject] else {
+            XCTAssert(false)
+            return
+        }
 
+        let remoteItem = RemoteMediaItem(dict: response)
+        let mediaItem = MediaItem(context: context)
+        mediaItem.site = site!
+        mediaItemStore.updateMediaItem(mediaItem, with: remoteItem)
+
+        CoreDataManager.shared.saveContext(context: context)
+
+        let retrievedItem = store!.getMediaItemWithID(mediaID: remoteItem.mediaID)!
+
+        XCTAssertEqual(mediaItem.objectID, retrievedItem.objectID)
     }
 
     func testHandleMediaFetchedAction() {
+        let context = CoreDataManager.shared.mainContext
+        let mediaItemStore = MediaItemStore()
+        guard let response = Loader.jsonObject(for: "remote-media-edit") as? [String: AnyObject] else {
+            XCTAssert(false)
+            return
+        }
 
+        // Set up MediaItem
+        //
+        let remoteItem = RemoteMediaItem(dict: response)
+        let mediaItem = MediaItem(context: context)
+        mediaItem.site = site!
+        mediaItemStore.updateMediaItem(mediaItem, with: remoteItem)
+        CoreDataManager.shared.saveContext(context: context)
+
+        let mediaID = remoteItem.mediaID
+
+        // Setup Media Action
+        let remoteMedia = RemoteMedia(dict: response)
+        let action = MediaFetchedApiAction(payload: remoteMedia, error: nil, image: UIImage(), previewURL: "http://example.com/image.png", mediaID: mediaID)
+
+        store?.handleMediaFetchedAction(action: action)
+
+        let expect1 = expectation(forNotification: .NSManagedObjectContextObjectsDidChange, object: CoreDataManager.shared.mainContext) { (_) -> Bool in
+            return true
+        }
+        wait(for: [expect1], timeout: 1)
+
+        let fetchRequest = Media.defaultFetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mediaID = %ld", mediaID)
+
+        let count = try! context.count(for: fetchRequest)
+        XCTAssertEqual(count, 1)
     }
 
 }
