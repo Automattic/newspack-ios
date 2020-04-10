79fe6af9163a326722fa6e197f60fd282b5db5a3
aerych <aerychj@gmail.com>
2020-04-10 15:57:29 -0500

    Folders: Removes AssetStore and related code.

--- Newspack/Newspack/Controllers/AssetsViewController.swift
+++ Newspack/Newspack/Controllers/AssetsViewController.swift
@@ -9,10 +9,10 @@ class AssetsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        items = StoreContainer.shared.assetStore.listAssets()
+//        items = StoreContainer.shared.assetStore.listAssets()
 
         receipt = StoreContainer.shared.folderStore.onChange { [weak self] in
-            self?.items = StoreContainer.shared.assetStore.listAssets()
+//            self?.items = StoreContainer.shared.assetStore.listAssets()
             self?.tableView.reloadData()
         }
     }
