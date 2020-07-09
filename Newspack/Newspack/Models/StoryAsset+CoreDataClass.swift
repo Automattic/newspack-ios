4edb9fd0fd0dbfb17cfebb12b20e7e184eca64a5
aerych <aerychj@gmail.com>
2020-07-09 14:19:38 -0500

    Data: Changes for sorting and ordering assets.

--- Newspack/Newspack/Models/StoryAsset+CoreDataClass.swift
+++ Newspack/Newspack/Models/StoryAsset+CoreDataClass.swift
@@ -4,6 +4,23 @@ import CoreData
 @objc(StoryAsset)
 public class StoryAsset: NSManagedObject {
 
+    public override func willSave() {
+        super.willSave()
+
+        updateSortedIfNeeded()
+    }
+
+
+    /// Called from willSave which will be called again if there are any changes
+    /// so only update the property if necessary.
+    ///
+    func updateSortedIfNeeded() {
+        let isSorted = order != -1
+        if sorted == isSorted {
+            return
+        }
+        sorted = isSorted
+    }
 }
 
 enum StoryAssetType: String {
