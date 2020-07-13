cfbfd082ad1a3fa2ed5584694cd96d9e8d08197b
aerych <aerychj@gmail.com>
2020-07-13 14:50:19 -0500

    Assets: Adds an action to change the sort mode.

--- Newspack/Newspack/Stores/Actions/AssetAction.swift
+++ Newspack/Newspack/Stores/Actions/AssetAction.swift
@@ -4,6 +4,7 @@ import WordPressFlux
 /// Supported Actions for changes to the AssetStore
 ///
 enum AssetAction: Action {
+    case sortMode(index: Int)
     case createAssetFor(text: String)
     case deleteAsset(assetID: UUID)
 }
