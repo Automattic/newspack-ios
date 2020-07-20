aa4641f951863901a5a169708403a8b406d28e5c
aerych <aerychj@gmail.com>
2020-07-20 13:54:28 -0500

    UI: Stubs a future menu. Adds left bar button to folders.

---
+++ Newspack/Newspack/Controllers/MenuViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
