d1c3351be2c8c1a787d329be7b119b2dcebefbb7
aerych <aerychj@gmail.com>
2020-07-22 14:24:00 -0500

    UI: Adds class for managing styling and appearance.

--- Newspack/Newspack/Styles/Appearance.swift
+++ Newspack/Newspack/Styles/Appearance.swift
@@ -1,9 +1,41 @@
-//
-//  Appearance.swift
-//  Newspack
-//
-//  Created by aerych on 7/20/20.
-//  Copyright © 2020 Automattic. All rights reserved.
-//
-
 import Foundation
+import UIKit
+
+class Appearance {
+
+    // MARK: - Tableview Styles
+
+    static func style(view: UIView, tableView: UITableView) {
+        view.backgroundColor = .basicBackground
+        tableView.backgroundColor = .neutral(.shade0)
+    }
+
+    static func style(cell: UITableViewCell) {
+        cell.backgroundColor = .basicBackground
+
+        cell.textLabel?.font = .tableViewText
+        cell.textLabel?.sizeToFit()
+
+        cell.detailTextLabel?.font = .tableViewSubtitle
+        cell.detailTextLabel?.sizeToFit()
+        // we only set the text subtle color, so that system colors are used otherwise
+        cell.detailTextLabel?.textColor = .textSubtle
+
+        cell.imageView?.tintColor = .neutral(.shade30)
+    }
+
+}
+
+// MARK: - Fonts
+
+extension UIFont {
+
+    static var tableViewText: UIFont {
+        .preferredFont(forTextStyle: .callout)
+    }
+
+    static var tableViewSubtitle: UIFont {
+        .preferredFont(forTextStyle: .callout)
+    }
+
+}
