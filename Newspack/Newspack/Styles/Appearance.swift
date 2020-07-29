7f98d36f24a302f0d84011d70eb1b8ef7163edf1
aerych <aerychj@gmail.com>
2020-07-29 09:28:44 -0500

    UI: Fix cell background in darkmode.

--- Newspack/Newspack/Styles/Appearance.swift
+++ Newspack/Newspack/Styles/Appearance.swift
@@ -6,8 +6,7 @@ class Appearance {
     /// Configure global UI appearance settings via the appearane API.
     ///
     static func configureGlobalAppearance() {
-        let view = UIView()
-        view.backgroundColor = .cellBackgroundSelected
+        let view = CellBackgroundView()
         UITableViewCell.appearance().selectedBackgroundView = view
     }
 
@@ -61,6 +60,27 @@ extension UIFont {
 
     static var tableViewSubtitle: UIFont {
         .preferredFont(forTextStyle: .callout)
+    }
+
+}
+
+// MARK: - Custom Views
+
+// Apparently views used for a cell's background do not have their traits updated
+// correctly. So we'll use this class to inspect the parent's traits to see if
+// we're in light or dark mode.
+class CellBackgroundView: UIView {
+
+    override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        guard let superView = newSuperview else {
+            return
+        }
+        if superView.traitCollection.userInterfaceStyle == .dark {
+            backgroundColor = .withColorStudio(.newspackBlue, shade: .shade80)
+        } else {
+            backgroundColor = .withColorStudio(.newspackBlue, shade: .shade20)
+        }
     }
 
 }
