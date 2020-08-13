71dbb9e8f53e50e86a739a957f02afe78a8ea7da
aerych <aerychj@gmail.com>
2020-08-13 16:41:38 -0500

    UI: Basic layout for Story cell

--- Newspack/Newspack/View/StoryTableViewCell.swift
+++ Newspack/Newspack/View/StoryTableViewCell.swift
@@ -2,15 +2,36 @@ import UIKit
 
 class StoryTableViewCell: UITableViewCell {
 
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var syncImage: UIImageView!
+    @IBOutlet var iconView: UIStackView!
+    @IBOutlet var textIcon: UIButton!
+    @IBOutlet var photosIcon: UIButton!
+    @IBOutlet var videoIcon: UIButton!
+    @IBOutlet var audioIcon: UIButton!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        applyStyles()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+
+    func applyStyles() {
+
+    }
+
+    func configure() {
+
     }
 
 }
