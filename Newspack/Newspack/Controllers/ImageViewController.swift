6a776ac7fb886440745af6813be98d9e637f6c9b
aerych <aerychj@gmail.com>
2020-08-19 15:58:24 -0500

    Photos: Stubs for photo detail and preview controllers

---
+++ Newspack/Newspack/Controllers/ImageViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
