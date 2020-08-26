c822185554940a104ce1624dad43e8342c6c578a
aerych <aerychj@gmail.com>
2020-08-26 10:38:01 -0500

    Photos: Make properties optional.

--- Newspack/Newspack/Models/AttachmentInfo+CoreDataProperties.swift
+++ Newspack/Newspack/Models/AttachmentInfo+CoreDataProperties.swift
@@ -11,8 +11,8 @@ extension AttachmentInfo {
     @NSManaged public var attachmentID: Int64
     @NSManaged public var caption: String!
     @NSManaged public var altText: String!
-    @NSManaged public var pageURL: URL!
-    @NSManaged public var srcURL: URL!
+    @NSManaged public var pageURL: URL?
+    @NSManaged public var srcURL: URL?
     @NSManaged public var asset: StoryAsset!
 
 }
