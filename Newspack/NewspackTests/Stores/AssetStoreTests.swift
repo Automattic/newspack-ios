d8d5047ea361583bf017c0a220f8b857d4a0fa01
aerych <aerychj@gmail.com>
2020-10-14 17:50:36 -0500

    Tests: Stubs AssetStore tests.

--- Newspack/NewspackTests/Stores/AssetStoreTests.swift
+++ Newspack/NewspackTests/Stores/AssetStoreTests.swift
@@ -1,33 +1,104 @@
-//
-//  AssetStoreTests.swift
-//  NewspackTests
-//
-//  Created by aerych on 10/14/20.
-//  Copyright © 2020 Automattic. All rights reserved.
-//
-
 import XCTest
+import CoreData
+@testable import Newspack
 
-class AssetStoreTests: XCTestCase {
+class AssetStoreTests: BaseTest {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var account: Account!
+    var site: Site!
+    var folderStore: FolderStore!
+    var assetStore: AssetStore!
+    var siteStore: SiteStore!
+
+    override func setUp() {
+        super.setUp()
+
+        // Test account and site
+        site = ModelFactory.getTestSite(context: CoreDataManager.shared.mainContext)
+
+        account = accountStore!.createAccount(authToken: "testToken", forNetworkAt: site.url)
+        site.account = account
+
+        CoreDataManager.shared.saveContext(context: CoreDataManager.shared.mainContext)
+
+        siteStore = SiteStore(dispatcher: testDispatcher, siteID: site.uuid)
+
+        folderStore = FolderStore(dispatcher: testDispatcher, siteID: site.uuid)
+
+        assetStore = AssetStore(dispatcher: testDispatcher)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        super.tearDown()
+
+        account = nil
+        site = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAssetNameFromString() {
+        let shortString = "This is a short test string"
+        let longString = "This is a long test string with some spaces abcdefghijklmnopqrstuvwxyz"
+        let longStringNoBreaks = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+        let shortName = assetStore.assetName(from: shortString)
+        let longName = assetStore.assetName(from: longString)
+        let longNameNoBreaks = assetStore.assetName(from: longStringNoBreaks)
+
+        XCTAssertEqual(shortName, shortString)
+        XCTAssertEqual(longName, "This is a long test string with some spaces...")
+        XCTAssertEqual(longNameNoBreaks, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX...")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testDeleteAsset() {
+
+    }
+
+    func testDeleteAssets() {
+
+    }
+
+    func testHandleDeletedRemoteMedia() {
+
+    }
+
+    func testUpdateCaption() {
+
+    }
+
+    func testUpdateAltText() {
+
+    }
+
+    func testUpdateAsset() {
+
+    }
+
+    func testGetAssetByID() {
+
+    }
+
+    func testGetAssetsForFolder() {
+
+    }
+
+    func testGetRemoteIDsForFolders() {
+
+    }
+
+    func testGetAssetsForFoldersWithIDs() {
+
+    }
+
+    func testGetAssetForFoldersWithRemoteID() {
+
+    }
+
+    func testGetAssetsWithChanges() {
+
+    }
+
+    func testGetAssetsNeedingUpload() {
+
     }
 
 }
