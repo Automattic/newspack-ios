8eb58b4b909b040345ddfdb0bb6a9e1e0f457874
aerych <aerychj@gmail.com>
2020-10-19 16:17:41 -0500

    Adds Array Helper and reorders some classes.

--- Newspack/NewspackFramework/Extensions/Array+Helpers.swift
+++ Newspack/NewspackFramework/Extensions/Array+Helpers.swift
@@ -8,3 +8,11 @@ extension Array {
         }
     }
 }
+
+// Props https://stackoverflow.com/a/46354989
+public extension Array where Element: Hashable {
+    func uniqued() -> [Element] {
+        var seen = Set<Element>()
+        return filter{ seen.insert($0).inserted }
+    }
+}
