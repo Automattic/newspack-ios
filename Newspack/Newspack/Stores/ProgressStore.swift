d3b0de67f0fdceccbfcb5d0645c7fa5ee248e525
aerych <aerychj@gmail.com>
2020-10-20 11:00:22 -0500

    Minor improvement and comments added to ProgressStore.

--- Newspack/Newspack/Stores/ProgressStore.swift
+++ Newspack/Newspack/Stores/ProgressStore.swift
@@ -1,6 +1,20 @@
 import Foundation
 import UIKit
 
+/// A simple class to wrap a UUID.  UUIDs are structs and we want value object
+/// to use with notifications. Hence this helper class.
+///
+class ProgressKey {
+    let uuid: UUID
+    init(uuid: UUID) {
+        self.uuid = uuid
+    }
+}
+
+/// Storage for UIProgress instances associated with a particular StoryAsset.
+/// Association is by UUID.  Interested observers should first get a ProgressKey
+/// for the UUID in question, then observe notifiations for that ProgressKey.
+///
 class ProgressStore {
 
     static let startedTrackingProgress = NSNotification.Name("startedTrackingProgress")
@@ -9,47 +23,73 @@ class ProgressStore {
     private var store = [UUID: Progress]()
     private var progressKeys = [UUID: ProgressKey]()
 
+    /// Get a progress object for the specified UUID if it exists.
+    ///
+    /// - Parameter uuid: A UUID.
+    /// - Returns: A Progress instance or nil.
+    ///
     func progress(for uuid: UUID) -> Progress? {
         return store[uuid]
     }
 
+    /// Add a Progress instance to the store for the specified UUID. This removes
+    /// an existing Progress instance from the store.
+    ///
+    /// - Parameters:
+    ///   - progress: A Progress instance.
+    ///   - uuid: The UUID.
+    ///
     func add(progress: Progress, for uuid: UUID) {
         remove(for: uuid) // remove stale progress if it exists.
         store[uuid] = progress
-        _ = keyForUUID(uuid: uuid)
-        notify(uuid: uuid, added: true)
+
+        let key = keyForUUID(uuid: uuid)
+        notify(key: key, added: true)
     }
 
+    /// Removes the Progress instance for the specified UUID.
+    ///
+    /// - Parameter uuid: The UUID.
+    ///
     func remove(for uuid: UUID) {
         guard let _ = store.removeValue(forKey: uuid) else {
             return
         }
+
+        // Get a reference to the key before removing it.
+        let key = keyForUUID(uuid: uuid)
         progressKeys.removeValue(forKey: uuid)
 
-        notify(uuid: uuid, added: false)
+        notify(key: key, added: false)
     }
 
-    private func notify(uuid: UUID, added: Bool) {
+    /// Posts notifications related to starting or stopping tracking progress for
+    /// the specified ProgressKey.
+    ///
+    /// - Parameters:
+    ///   - key: A ProgressKey instancce.
+    ///   - added: Whether progress is being tracked or not.
+    ///
+    private func notify(key: ProgressKey, added: Bool) {
         let name = added ? ProgressStore.startedTrackingProgress : ProgressStore.stoppedTrackingProgress
-        let obj = keyForUUID(uuid: uuid)
-        NotificationCenter.default.post(name: name , object: obj)
+        NotificationCenter.default.post(name: name , object: key)
     }
 
+    /// Get a ProgressKey for the specified UUID.
+    ///
+    /// - Parameter uuid: The UUID.
+    /// - Returns: A ProgressKey instance.
+    ///
     func keyForUUID(uuid: UUID) -> ProgressKey {
-        if let obj = progressKeys[uuid] {
-            return obj
+        // Just return an existing key.
+        if let key = progressKeys[uuid] {
+            return key
         }
 
-        let obj = ProgressKey(uuid: uuid)
-        progressKeys[uuid] = obj
-        return obj
+        // Create a key and return it.
+        let key = ProgressKey(uuid: uuid)
+        progressKeys[uuid] = key
+        return key
     }
 
-}
-
-class ProgressKey {
-    let uuid: UUID
-    init(uuid: UUID) {
-        self.uuid = uuid
-    }
 }
