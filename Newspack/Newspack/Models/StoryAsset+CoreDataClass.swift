8bc11f2e9f10538685dcd7d13401400c846d965d
aerych <aerychj@gmail.com>
2020-10-22 18:28:54 -0500

    Data: Remove unused fields from StoryAsset

--- Newspack/Newspack/Models/StoryAsset+CoreDataClass.swift
+++ Newspack/Newspack/Models/StoryAsset+CoreDataClass.swift
@@ -5,22 +5,6 @@ import CoreServices
 @objc(StoryAsset)
 public class StoryAsset: NSManagedObject, TextNoteCellProvider, PhotoCellProvider, VideoCellProvider, AudioCellProvider {
 
-    public override func willSave() {
-        super.willSave()
-
-        updateSortedIfNeeded()
-    }
-
-    /// Called from willSave which will be called again if there are any changes
-    /// so only update the property if necessary.
-    ///
-    func updateSortedIfNeeded() {
-        let isSorted = order != -1
-        if sorted == isSorted {
-            return
-        }
-        sorted = isSorted
-    }
 }
 
 enum StoryAssetType: String {
--- Newspack/Newspack/Models/StoryAsset+CoreDataProperties.swift
+++ Newspack/Newspack/Models/StoryAsset+CoreDataProperties.swift
@@ -21,14 +21,12 @@ extension StoryAsset {
     @NSManaged public var bookmark: Data? // Text notes do not have local files. RemoteMedia may not have local files.
     @NSManaged public var name: String!
     @NSManaged public var uuid: UUID!
-    @NSManaged public var order: Int16 // Default is -1
     @NSManaged public var date: Date!
     // Synced indicates the date last synced.
     @NSManaged public var synced: Date!
     // Modified indicate the date last modified. If synced > modified data is current.
     @NSManaged public var modified: Date!
     @NSManaged public var text: String!
-    @NSManaged public var sorted: Bool
     @NSManaged public var altText: String!
     @NSManaged public var caption: String!
     @NSManaged public var remoteID: Int64
